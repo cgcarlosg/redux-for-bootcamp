d943f0eef13a4c823f9ff1eff96dac00065bf762
cgcarlosg <cgcarlosg@hotmail.com>
2021-07-05 09:46:14 -0500

    Connect state and API request

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,4 @@
-export const getAll = async () => {
+export const getMovies = async () => {
     const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=d32935c9ba902f453e2cff810df3712a';
     const request = await fetch(url, { mode: 'cors' });
     if (request.status === 200) {
--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,11 +1,18 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import store from '../redux/store'
+// fetchMoviesFailure, fetchMoviesLoading
+import {useEffect} from 'react';
+import moviesRequest from '../redux/thunkFunction'
+
 const MovieList = props => {
     console.log(store.getState())
+    useEffect(() => {
+        moviesRequest();
+    });
     return (
         <div>
-            <h1>I am a list</h1>
+            <h1>{store.requesting}</h1>
         </div>
     )
 }
