74b25ea06032bcd55145d9ebaf352ccceff31a2b
cgcarlosg <cgcarlosg@hotmail.com>
2021-07-05 09:13:50 -0500

    Add movielist with api key in component folder

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Reacr from 'react';
+import MovieList from '../containers/MovieList';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+      <h1>Init Application</h1>
+
+    <MovieList />
+    </div>
+  );
+}
+
+export default App;
