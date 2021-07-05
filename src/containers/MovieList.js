0be2dcac230899e0b8aa784c605f9adcf7215d8e
cgcarlosg <cgcarlosg@hotmail.com>
2021-07-05 11:10:05 -0500

    Set category filter component

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -6,6 +6,7 @@ import moviesRequest from '../redux/thunkFunction';
 import { connect } from 'react-redux';
 import {getterMoviesWithInfo} from "../redux/getters";
 import MovieCard from '../components/MovieCard';
+import GenreFilter from '../components/GenreFilter';
 
 const MovieList = ({ moviesList }) => {
     console.log(store.getState())
@@ -18,12 +19,12 @@ const MovieList = ({ moviesList }) => {
     }, []);
     return (
         <div>
+           <GenreFilter></GenreFilter>
             {moviesList && moviesList.length
           ? moviesList.map(movie => (
-             
             <MovieCard movie={movie} key={movie.id}></MovieCard>
           ))
-          : (<h1>No movies yet! :(</h1>)}
+          : (<h1>No books yet! :(</h1>)}
         </div>
     )
 }
